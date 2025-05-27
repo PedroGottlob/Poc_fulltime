a10443a332c8dbba89889862bff7a57bf4a1a716
pedro.bech <pedrogottlob@gmail.com>
2025-05-27 15:46:58 -0300

    sim

--- src/main/java/org/example/config/RabbitConfig.java
+++ src/main/java/org/example/config/RabbitConfig.java
@@ -1,15 +1,37 @@
 package org.example.config;
 
+import org.springframework.amqp.core.AmqpAdmin;
+import org.springframework.amqp.core.Binding;
+import org.springframework.amqp.core.DirectExchange;
 import org.springframework.amqp.core.Queue;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class RabbitConfig {
-    public static final String FILA_DADOS = "fila.dados";
+    public static final String FILA_DADOS = "amq.direct";
+
+
 
     @Bean
-    public Queue filaDados() {
-        return new Queue(FILA_DADOS, true);
+    public Queue filaDados(String nomeFila)
+    {
+        return new Queue(FILA_DADOS, true,false ,false);
     }
+
+    private DirectExchange trocaDireta(){
+        return new DirectExchange(FILA_DADOS);
+    }
+    private Binding relacionamento (Queue fila,DirectExchange troca){
+      return  new Binding(fila.getName(), Binding.DestinationType.EXCHANGE, troca.getName(), FILA_DADOS, null);
+    }
+
+     private void adiciona(){
+        Queue filaDados =  this.filaDados(FILA_DADOS);
+
+        DirectExchange troca = this.trocaDireta();
+
+        this.relacionamento(filaDados, troca);
+     }
+
 }
